573d17437ffbc77635fe0e06ed68f8722e62afa3
Incursor <incursortrasgo@gmail.com>
2023-08-10 23:00:35 -0300

    Update GrowBox.py

--- GrowBox.py
+++ GrowBox.py
@@ -20,8 +20,9 @@ humedad = 0
 def interrup_rst(pin):
     if pin_pulsador.value() == 0:
         print("Pulsador presionado, reiniciando...")
-        # machine.reset()  # Reinicia el ESP32
+        machine.reset()  # Reinicia el ESP32
         # pin_r1.value(not pin_r1.value())
+
 
 # Configura la interrupción en el pin del pulsador
 pin_pulsador.irq(trigger=machine.Pin.IRQ_FALLING, handler=interrup_rst)
@@ -30,17 +31,18 @@ pin_pulsador.irq(trigger=machine.Pin.IRQ_FALLING, handler=interrup_rst)
 tim0 = Timer(0)
 # interrupcion a la que llama el timer 0
 def interrup_t0(tim0):
-    print("disparo timer 0")
     try:
         sensor.measure()
         global temperatura
         global humedad
         temperatura = sensor.temperature()
         humedad = sensor.humidity()
-        print(temperatura)
-        print(humedad)
+#         print(temperatura)
+#         print(humedad)
     except OSError as e:
         print("error sensor", e)
+
+
 # inicializa el timer
 tim0.init(period=2000, mode=Timer.PERIODIC, callback=interrup_t0)
 
@@ -66,33 +68,74 @@ HTTP/1.1 200 OK
     <meta charset="UTF-8">
     <title>GrowBox [beta]</title>
 </head>
+<style>
+    body {{
+        background-image: url(
+        "https://lh3.googleusercontent.com/pw/AIL4fc8aqICJOGtWal8Q1Ghbze4NhdCun-2Elm36Sf0jyHnGVepzN9qDblrD104rAtDmtDG_7fl8nsMEs-BRef2YvkHvZTv4FexcyMezTowz7IykpQCsLbNv7mWwdOe3-0p8kGSoskQE7FUPEhYx-Yco7ptUVg=w748-h1580-s-no"
+        )
+    }}
+    .container {{
+        position: relative;
+        text-align: center;
+        color: white;
+    }}
+</style>
+
 <body>
+    <body>
+    <div class="container">
     <h2>Bienvenido a GrowBox</h2>
     <p>Datos de ambiente:</p>
-    <p>Temperatura: {:.2f} °C</p>
-    <p>Humedad: {:.2f} %</p>
+    <p>Temperatura: {:.1f} °C</p>
+    <p>Humedad: {:.1f} %</p>
     <p>Accionamiento Rele N°1</p>
     <form method="POST" action="/">
-        <button name="boton" value="presionado" type="submit">Conmutar R1</button>
+        <button name="boton" value="pres" type="submit">Conmutar R1</button>
     </form>
+    </div>
 </body>
 </html>
-""".format(temperatura, humedad)
+""".format(
+        temperatura, humedad
+    )
 
     client_socket.send(response.encode("utf-8"))
-    request = client_socket.recv(1024)
-    if "boton=presionado" in request:
-        print("Botón en la página web presionado.")
-        pin_r1.value(not pin_r1.value())
 
-    client_socket.close()
+
+def toggle_pin():
+    pin_r1.value(not pin_r1.value())
+    print("Toggle pin")
+
+
+# Binding to all interfaces - server will be accessible to other hosts!
+ai = usocket.getaddrinfo("0.0.0.0", 8585)
+# print("Bind address info:", ai)
+addr = ai[0][-1]
 
 server = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
 server.bind(("192.168.18.168", 80))
 server.listen(5)
 
+
+def routing(client_socket):
+    """
+    decodea la solicitud y enruta a la función correspondiente
+    """
+    request = client_socket.recv(1024)
+    request = request.decode().replace("\r\n", "\n")
+    print("Contenido de la solicitud: {}".format(str(request)))
+
+    if request.find("GET / HTTP/1.1") != -1:
+        http_handler(client_socket)
+    elif request.find("POST / HTTP/1.1") != -1 and request.find("boton=pres") != -1:
+        # si es un POST y viene el valor del boton, hacer toggle del pin
+        toggle_pin()
+        http_handler(client_socket)
+
+
 while True:
     # Acepta las solicitudes de los clientes y maneja las respuestas
     client, addr = server.accept()
-    print("Respuesta a cliente")
-    http_handler(client)
+    print("Routing")
+    routing(client)
+    client.close()
